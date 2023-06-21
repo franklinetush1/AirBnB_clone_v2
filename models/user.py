b3b1f8aaf4bd47c2e1d7bd2d26fdeca6ee24fe46
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-21 08:32:16 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel):
