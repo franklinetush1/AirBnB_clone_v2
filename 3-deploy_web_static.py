141fbd0c6f7d660ae79cb347fffae9ba7f91cfc8
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-06 21:12:35 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+from fabric import task
+from datetime import datetime
+
+def do_pack():
+    """Returns the path of the generated archive if successful,else None"""
+    time_st = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(time_st)
+    local("mkdir -p versions")
+  	output = local("tar -czvf versions/{} web_static".format(archive_name))
+	archive_path = "versions/{}."format(archive_name)
+    if output.failed:
+        return None
+    return archive_path
+
+
 
 def deploy():
     """Creates and distributes an archive to the web servers"""
