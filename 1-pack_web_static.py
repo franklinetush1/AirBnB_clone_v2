8c878e1f664fe0e014fb86701c3b8c377bff4bfd
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-06 19:03:22 +0300

    Update 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+ """Generates a .tgz archive from the contents of the web_static folder."""
+from fabric import task
+from datetime import datetime
+def do_pack():
+    """Returns the path of the generated archive if successful,else None"""
+    time_st = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(time_st)
+    local("mkdir -p versions")
+  	output = local("tar -czvf versions/{} web_static".format(archive_name))
+	file_path = "versions/{}."format(archive_name)
+    if output.failed:
+        return None
+    return file_path
