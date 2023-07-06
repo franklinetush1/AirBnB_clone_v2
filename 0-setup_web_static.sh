38bf476c946152f4bc0d9175ff9efee493f338b7
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-06 18:48:20 +0300

    Update 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# script that sets up the web servers for deployment
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "25i\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx
