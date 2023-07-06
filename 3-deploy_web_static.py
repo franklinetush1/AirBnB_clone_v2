b827b22378f9d17fed71b7d622aab632b66e365a
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-06 21:14:02 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from fabric import task
 from datetime import datetime
+from fabric.api import *
+import os
 
 def do_pack():
     """Returns the path of the generated archive if successful,else None"""
@@ -13,7 +15,29 @@ def do_pack():
         return None
     return archive_path
 
+env.hosts = ['54.236.50.142', '52.87.231.81']
+env.user = "ubuntu"
 
+
+def do_deploy(archive_path):
+    """Distributes an archive to the web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        archive_folder = archive_filename.split(".")[0]
+        put(archive_path, '/tmp/')        
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, archive_folder))        
+        run('rm /tmp/{}'.format(filename))        
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_folder, archive_folder))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_folder))        
+        run('rm -rf /data/web_static/current')        
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_folder))
+        print("New version deployed!")
+        return True
+    except:
+        return False
 
 def deploy():
     """Creates and distributes an archive to the web servers"""
