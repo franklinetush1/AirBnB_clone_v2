fd0c95790ff20212892c1eabbbccb09d4896a5ec
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-09 23:11:57 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,17 @@ import os
 env.hosts = ['54.236.50.142', '52.87.231.81']
 env.user = "ubuntu"
 
+def do_pack():
+    """Returns the path of the generated archive if successful,else None"""
+    time_st = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(time_st)
+    local("mkdir -p versions")
+    output = local("tar -czvf versions/{} web_static".format(archive_name))
+    archive_path = "versions/{}".format(archive_name)
+    if output.failed:
+        return None
+    return archive_path
+
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
