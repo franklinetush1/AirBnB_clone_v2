a89f030094788501b65f5b78360c1ebeed7bd02c
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-21 19:30:45 +0300

    Flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Runs an app with flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/',strict_slashes=False)
+
+def hello():
+  """Returnn 'Hello HBNB!'"""
+    return “Hello HBNB!”
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
