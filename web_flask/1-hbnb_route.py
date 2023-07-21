40d817e023751cd33e440874ada709293f339e04
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-21 19:33:59 +0300

    new route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Runs an app with flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',strict_slashes=False)
+def hello():
+  """Returnn 'Hello HBNB!'"""
+    return “Hello HBNB!”
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
