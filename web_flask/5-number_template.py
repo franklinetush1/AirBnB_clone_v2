97da2c0f3fce9f760ea6364cf41fed76eb21ee1c
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-21 21:44:35 +0300

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,7 @@ def python(text="is cool"):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """ Function called with /number_template/<n> route """
+    """Renders a page"""
     return render_template('5-number.html', num=n)
 
 if __name__ == "__main__":
