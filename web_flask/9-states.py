72e33caa7f5d497bde2aac19611ae63a8e8d81a9
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-22 18:28:44 +0300

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def session(exception):
+    """Close session"""
+    storage.close()
+
+@app.route('/states', strict_slashes=False)
+def Display_states():
+    """Display html"""
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+@app.route('/states', strict_slashes=False)
+def states_id(id):
+    """Display html with id"""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
