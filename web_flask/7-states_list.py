6c57a80a2a26f6014a82eda484756c8ee122b6c2
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-22 18:31:49 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,12 +4,12 @@ from models import storage
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def teardown(excep):
+def teardown(err):
     """Delete the current session."""
     storage.close()
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
+def states():
     """Display states"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
