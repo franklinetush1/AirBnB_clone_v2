a69c4622d944594f87c1d02493171187abeb6419
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-22 12:59:27 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """Remove session"""
+    storage.close()
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display html page"""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
