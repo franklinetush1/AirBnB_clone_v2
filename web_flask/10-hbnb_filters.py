fa7654ccddc6c26872dc3f40b348651ce81f1362
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-22 19:18:43 +0300

    Update 10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Starts a Flask application"""
+from models import storage, render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displays the filters"""
+    amen = storage.all("Amenity")
+    st = storage.all("State")
+    return render_template("10-hbnb_filters.html",
+                           states=st, amenities=amen)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
