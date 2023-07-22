8652a371669fc87a9c6d0e5da7dca32011940cf0
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-22 10:21:37 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,7 @@ class FileStorage:
                 del self.__objects[key]
         except(AttributeError, KeyError):
             pass
+
+    def close(self):
+        """ calls reload method"""
+        self.reload()
